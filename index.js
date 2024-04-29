bb1d78800ea2eb14b7b5953ab78836d1b549a13b
Rashmi95251 <rashmiraj2312@gmail.com>
2024-04-30 00:09:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,26 @@
 //----------------------------------------------------------------//
-//Main Entry Point of the Express Server App//
+//Index Root of all the Routes//
 //----------------------------------------------------------------//
-//Require Express Module for running the Express Server
+//Require the existing Express
 const express = require("express");
-//Create Port
-const port = process.env.PORT || 8000;
-//Create Express App for Request-Response Cycle & to create the Express Server
-const app = express();
-//Require Module Path for Directory
-const path = require("path");
-//Requires the index.js - Route File, from the Routes Folder
-const route = require("./routes/index.js");
-//Requires express-ej-layouts Module
-const expressLayouts = require("express-ejs-layouts");
-//Requires MongoDB
-const db = require("./config/mongoose.js");
-//Requires cors Module
-const cors = require("cors");
-//Requires Dotenv Module
-const dotenv = require("dotenv").config();
+//Create a Local Router
+const router = express.Router();
 
+//Require Home Router File
+const homeRouterFile = require("./home");
+//Require Tasks Router File
+const taskRouterFile = require("./tasks");
+//Require Tasks List Router File
+const taskListRouterFile = require("./task-list");
 
-//Use the Cors Module
-app.use(cors());
-//Middleware - Express App uses Static Files in the Assets Folder
-app.use(express.static("./assets"));
-//Middleware - Express App uses expressLayouts to tell that the views which are going to be rendered belongs to some layout
-app.use(expressLayouts);
+//Access the Home Router File on '/' route
+router.use("/", homeRouterFile);
+//Access the Tasks List Router File on '/task-list' route
+router.use("/task-list", taskListRouterFile);
+//Access the Tasks Router File on '/tasks' route
+router.use("/tasks", taskRouterFile);
 
-//Set Up - Extract Styles and Scripts from Sub Pages into the Layout
-app.set("layout extractStyles", true);
-//Set Up - Extract Styles and Scripts from Sub Pages into the Layout
-app.set("layout extractScripts", true);
-
-//Middleware - URL Encoder
-app.use(express.urlencoded({ extended: true }));
-//Middleware - App calls index.js - Route File, whenever '/' route is called in the request
-app.use("/", route);
-
-//Set Up - Template Engine as EJS
-app.set("view engine", "ejs");
-//Set Up - Template Engine Views Folder Path (..../views)
-app.set("views", path.join(__dirname, "views"));
-
-//Run the ExpressJS Server
-app.listen(port, (err) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	console.log(`Server is Up & Running Successfully on Port ${port}`);
-});
+//Main Index Router Loaded
+console.log("Router Loaded");
+//Export the Index Router
+module.exports = router;
